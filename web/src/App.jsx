e1383d1c0c2385f5e81062d66d22d58ff6d506f4
Anca Maria Sirbu <ancamaria_sirbu@yahoo.com>
2025-11-04 17:05:42 +0100

    chore: react router

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,8 +1,12 @@
 import './App.css'
-import Home from "./pages/Home";
+import { Outlet } from "react-router-dom";
 
 function App() {
-  return <Home />;
+  return (
+    <div>
+      <Outlet />
+    </div>
+  );
 }
 
 export default App;
--- web/src/main.jsx
+++ web/src/main.jsx
@@ -2,9 +2,36 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import Home from "./pages/Home";
+import City from "./pages/City";
+import Favorites from "./pages/Favorites.jsx";
+import NotFoundPage from "./pages/NotFoundPage.jsx";
+import{createBrowserRouter, RouterProvider} from "react-router-dom";
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      { index: true, element: <Home /> }, // 👈 default route
+      { path: "city/:cityName", element: <City /> },
+      { path: "favorites", element: <Favorites /> },
+      { path: "*", element: <NotFoundPage /> },
+    ],
+  },
+]);
+
+// const router = createBrowserRouter([
+//   {path: "/", element: <Home />},
+//   {path:"/city/:name", element: <City />},
+//   {path:"/favorites", element: <Favorites />},
+//   {path:"*", element: <NotFoundPage />},
+// ]);
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )

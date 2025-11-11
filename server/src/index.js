b787a504b8225087458123162e0be59b8370c2af
Anca Maria Sirbu <ancamaria_sirbu@yahoo.com>
2025-11-11 12:36:48 +0100

    chore: future routes - file structure

--- server/src/index.js
+++ server/src/index.js
@@ -2,10 +2,17 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const PORT = 4000;
+const weatherRouter = require("./routes/weather");
+const forecastRouter = require("./routes/forecast");
 
 app.use(cors({
   origin: "http://localhost:5173"
 }));
+
+app.use('/api/weather', weatherRouter);
+app.use('/api/forecast', forecastRouter);
+
+
 
 app.get("/api/health", (req, res) => {
   res.json({ ok: true });
@@ -14,6 +21,7 @@ app.get("/api/health", (req, res) => {
 app.get("/api/hello", (req, res) => {
   res.json({ message: "Hello from server" });
 })
+
 
 
 app.listen(PORT, () => {

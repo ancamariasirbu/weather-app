4d4165cef59bf1e730fb20a8a8f08b136a257fd0
Anca Maria Sirbu <ancamaria_sirbu@yahoo.com>
2025-11-24 13:54:09 +0100

    feat: add loader component

---
+++ web/src/components/Loader.jsx
@@ -0,0 +1,11 @@
+import "./Loader.css";
+
+function Loader() {
+  return (
+    <div className="loader" aria-label="loading">
+      <div className="loader-circle" />
+    </div>
+  );
+}
+
+export default Loader;

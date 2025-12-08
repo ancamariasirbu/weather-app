ab645a04c1234b5b85e53c160a122c339dedf26f
Anca Maria Sirbu <ancamaria_sirbu@yahoo.com>
2025-12-08 20:35:54 +0100

    feat: update RATE_LIMIT_MAX_REQUESTS to allow 100 requests per IP per window

--- server/src/config.js
+++ server/src/config.js
@@ -1,4 +1,4 @@
 module.exports = {
   RATE_LIMIT_WINDOW_MS: 60 * 1000, // 1 minute
-  RATE_LIMIT_MAX_REQUESTS: 60      // max 60 requests per IP per window
+  RATE_LIMIT_MAX_REQUESTS: 100, // max 60 requests per IP per window
 };
